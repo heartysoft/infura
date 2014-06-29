108837a7fedbbe129aafc08f51a6f503490f9fbd
ashic <ashic@live.com>
2014-06-29 09:52:23 +0100

    in mem es now supports proper dispatching

--- src/Infura/Infura/EventSourcing/InMemoryEventStore.cs
+++ src/Infura/Infura/EventSourcing/InMemoryEventStore.cs
@@ -1,31 +1,67 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Infura.EventSourcing
 {
     public class InMemoryEventStore : EventStore
     {
-        readonly Dictionary<object, List<object>> _store = new Dictionary<object, List<object>>();
+        readonly Dictionary<object, List<StoredEvent>> _store = new Dictionary<object, List<StoredEvent>>();
+        private Func<DateTime> _clock = () => DateTime.UtcNow;
+        private Action<StoredEvent> _dispatcher = x => { };
 
         public void StoreEvents(object id, IEnumerable<object> events, long expectedInitialVersion)
         {
+            var storedEvents = getStoredEvents(id, events, expectedInitialVersion);
+
             if (_store.ContainsKey(id))
-                _store[id].AddRange(events);
+                _store[id].AddRange(storedEvents);
             else
-                _store[id] = new List<object>(events);
+                _store[id] = new List<StoredEvent>(storedEvents);
+
+            foreach (var @event in storedEvents)
+            {
+                _dispatcher(@event);
+            }
         }
 
         public IEnumerable<object> LoadEvents(object id, long version = 0)
         {
             if (_store.ContainsKey(id))
-                return _store[id];
+                return _store[id].Select(x => x.EventData).ToArray();
 
             return new object[0];
         }
         
         public void AdjustDispatcher(Action<StoredEvent> dispatcher)
         {
-            throw new NotImplementedException();
+            _dispatcher = dispatcher;
+        }
+
+        public void AdjustClock(Func<DateTime> clock)
+        {
+            _clock = clock;
+        }
+
+        private StoredEvent[] getStoredEvents(object id, IEnumerable<object> events, long expectedInitialVersion)
+        {
+            var existing = _store.ContainsKey(id);
+
+            if(existing == false && expectedInitialVersion != 1)
+                throw new ConcurrencyException(id.ToString(), null, expectedInitialVersion);
+
+            var existingMaxVersion = existing? _store[id].Max(x => x.Sequence) : 0;
+
+            if(expectedInitialVersion != existingMaxVersion+1)
+                throw new ConcurrencyException(id.ToString(), existingMaxVersion, expectedInitialVersion);
+
+            int offset = 0;
+            return events.Select(x =>
+            {
+                var version = expectedInitialVersion + offset;
+                offset++;
+                return new StoredEvent(Guid.NewGuid(), id, version, _clock(), x);
+            }).ToArray();
         }
     }
 }
